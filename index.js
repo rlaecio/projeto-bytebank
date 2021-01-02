f076b0c89d0da3648a8dfde008b3013078864e8b
Gitlab.network <roque.souza@portodigital.pt>
2021-01-02 19:11:33 +0000

    herança em js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 
 const cliente1 = new Cliente("Ricardo", 12345678910);
@@ -8,13 +9,14 @@ const cliente2 = new Cliente("Alice", 12345678911);
 console.log(cliente1, cliente2);
 
 const conta1 = new ContaCorrente(1001, cliente1);
+const conta2 = new ContaPoupanca(100, 1001, cliente1);
 conta1.depositar(500);
 
-const conta2 = new ContaCorrente(102, cliente2);
+const conta3 = new ContaCorrente(102, cliente2);
 conta1.transferir(100, conta2);
 
+conta1.sacar(100);
 console.log(conta1);
-console.log(conta2);
 
-console.log(ContaCorrente.numeroDeContas);
+
 
